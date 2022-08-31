d7f17367c72826eeec9822ddc0112012aeda1401
Sean Hickey <seanpathickey@gmail.com>
2022-08-30 20:20:55 -0500

    Section 4: 32-38

--- challenges/views.py
+++ challenges/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
+from django.template.loader import render_to_string
 
 monthly_challenges = {
     'january':'month 1',
@@ -47,7 +48,9 @@ def monthly_challenge_by_number(request, month):
 def monthly_challenge(request, month):
     try:
         challenge_text = monthly_challenges[month]
-        response_text = f'<h1>{challenge_text}</h1>'
-        return HttpResponse(response_text)
+        return render(request, 'challenges/challenge.html',{
+            'text': challenge_text,
+            'month': month
+        })
     except:
         return HttpResponseNotFound('<h1>You entered an invalid month</h1>')
