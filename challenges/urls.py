139303fe8d633babea0d67bffa5e05710bec0d01
Sean Hickey <seanpathickey@gmail.com>
2022-09-14 19:07:18 -0500

    Sections 43-45

--- challenges/urls.py
+++ challenges/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='index'), #use name to reference in other places
     #creates a dynamic url: (type:variable, file.function, name)
     path('<int:month>', views.monthly_challenge_by_number), 
     path('<str:month>', views.monthly_challenge, name='month-challenge') 
